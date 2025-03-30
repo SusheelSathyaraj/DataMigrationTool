52a6b7b611d048d114a97dd4bdec85b13c4157d7
Susheel Sathyaraj <susheel.sathyaraj@gmail.com>
2025-03-31 01:04:27 +0530

    implements the fetchdata functionality for the mysql database

--- database/mysql.go
+++ database/mysql.go
@@ -28,3 +28,64 @@ func ConnectMySQL() (*sql.DB, error) {
 	fmt.Println("Successfully connected to MySQL database... ")
 	return db, err
 }
+
+// retreives data from the mysql table and returns it has a slice of maps
+func FetchData(db *sql.DB) ([]map[string]interface{}, error) {
+	//to do, make this query generic so that hardcoding can be avoided,
+	query := "SELECT * FROM Employees;" // using 'users' as the hardcoded table name
+
+	//execute query
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute the query %v", err)
+	}
+	defer rows.Close()
+
+	//get column names
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get column names %v", err)
+	}
+
+	// store results in slice
+	var results []map[string]interface{}
+
+	//Iterating through the rows
+	for rows.Next() {
+		//placeholder interfaces to store values
+		values := make([]interface{}, len(columns))
+		valuesPtr := make([]interface{}, len(columns))
+
+		//storing corresponding pointer values as row.Scan() works only on pointers
+		for i := range values {
+			valuesPtr[i] = &values[i]
+		}
+
+		//scan the rows into the valuePtr
+		//valuesPtr... expands the slice into individual args
+		if err := rows.Scan(valuesPtr...); err != nil {
+			return nil, fmt.Errorf("failed to scan the row %v", err)
+		}
+
+		//create a new map to store the scanned data of the current row
+		rowMap := make(map[string]interface{})
+
+		//add column values for this row
+		for i, colName := range columns {
+			val := values[i]
+
+			//check to see if some values are in bytes, if yes convert them to string
+			if b, ok := val.([]byte); ok {
+				rowMap[colName] = string(b)
+			} else {
+				rowMap[colName] = val
+			}
+		}
+
+		//appending the row to results
+		results = append(results, rowMap)
+	}
+
+	//return all rows
+	return results, nil
+}
