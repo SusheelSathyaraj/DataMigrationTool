073a9910e72a22acde6ff8fa56752ba29cfcfe91
Susheel Sathyaraj <susheel.sathyaraj@gmail.com>
2025-04-11 15:31:16 +0530

    adds the connect to postgresql method

--- database/postgres.go
+++ database/postgres.go
@@ -1,10 +1,28 @@
 package database
 
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/SusheelSathyaraj/DataMigrationTool/config"
+)
+
 type PostgresMigration struct {
 }
 
-func ConnectPostgres() error {
-	return nil
+func ConnectPostgres(cfg config.PostgreSQLConfig) (*sql.DB, error) {
+	//Format: "host=localhost port=5432 user=postgres password=mysecretpassword dbname=mydb sslmode=disable"
+	dsn := fmt.Sprintf("host=%s port=%d user =%s password =%s dbname=%s sslmode=disable", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open postgresql connection,%v", err)
+	}
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping postgres, %v", err)
+	}
+	fmt.Println("Successfully connected to PostgreSQL database...!")
+	return db, nil
 }
 
 func (p *PostgresMigration) InsertData(data []map[string]interface{}) error {
