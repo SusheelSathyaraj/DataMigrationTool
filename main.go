121ac142a4074ab06f8703bc1ac73f15a09d99f9
Susheel Sathyaraj <susheel.sathyaraj@gmail.com>
2025-05-04 00:52:05 +0530

    updates code to accomodate refactored mysql.go code

--- main.go
+++ main.go
@@ -56,11 +56,6 @@ func isValidDatabase(db string, slice []string) bool {
 }
 
 func main() {
-	//Loading config from config.yaml
-	cfg, err := config.LoadConfig("config.yaml")
-	if err != nil {
-		log.Fatalf("Error loading config %v", err)
-	}
 
 	//defining CLI for user input
 	sourceDB := flag.String("source", "", "Source Database type(mysql,postgresql,mongodb)")
@@ -68,9 +63,16 @@ func main() {
 	mode := flag.String("mode", "full", "Migration mode(full,incremental,scheduled)")
 	//filetype := flag.String("filetype", "", "Format (csv,json,xml)")
 	//filetype to be added later
+	configPath := flag.String("config", "config.yaml", "Path to config file")
 
 	//parsing the user input
 	flag.Parse()
+
+	//Loading config from config.yaml
+	cfg, err := config.LoadConfig(*configPath)
+	if err != nil {
+		log.Fatalf("Error loading config %v", err)
+	}
 
 	//validate input
 	if err := validateInput(*sourceDB, *targetDB, *mode); err != nil {
@@ -81,21 +83,72 @@ func main() {
 	fmt.Println("Input validated successfully")
 	fmt.Printf("Starting Migration from %s to %s in %s mode", *sourceDB, *targetDB, *mode)
 
-	//checking the connection to mysql database
-	fmt.Println("\n Attempting to connect to MySQL database...")
-	db, err := database.ConnectMySQL(cfg.MySQL)
-	if err != nil {
-		log.Fatalf("connection failed %v", err)
-	}
-	defer db.Close()
-	fmt.Println("Connection Successful")
+	//var db *sql.DB
 
-	//checking the fetch functionality of the mysql database
-	fmt.Println("\n Fetching data from the mysql database...")
-	data, err := database.FetchData(db, cfg.SQLFilePath)
+	//checking the connection to database
+	fmt.Printf("\n Attempting to connect to %s database...", *sourceDB)
+
+	var sourceClient database.DatabaseClient
+
+	switch strings.ToLower(*sourceDB) {
+	case "mysql":
+		sourceClient = database.NewMYSQLClientFromConfig(cfg)
+	case "postgresql":
+		//TO DO: sourceClient = database.NewPostgreSQLFromConfig(cfg)
+		log.Fatal("PostgresQL not yet implemented")
+	default:
+		log.Fatalf("Unsupported source database type, %s", *sourceDB)
+	}
+
+	if err := sourceClient.Connect(); err != nil {
+		log.Fatalf("Failed to connect to %s Database, %v", *sourceDB, err)
+	}
+
+	//Using type ascertion to get correct client type
+	mysqlClient, ok := sourceClient.(*database.MySQLClient)
+	if !ok {
+		log.Fatalf("Failed to cast source client to expected type")
+	}
+
+	defer mysqlClient.Close()
+
+	fmt.Printf("Successfully connected to %s database", *sourceDB)
+
+	//Parsing SQL file
+	fmt.Println("Fetching data from source database...")
+	parser := &database.SQLParser{}
+	tables, err := parser.ParseSQLFiles(cfg.SQLFilePath)
+	if err != nil {
+		log.Fatalf("could not parse the SQL file, %v", err)
+	}
+
+	if len(tables) == 0 {
+		log.Fatalf("No tables found in the SQL file")
+	}
+
+	fmt.Printf("Found %d tables in %v ", len(tables), tables)
+
+	// fetch functionality of the mysql database tables
+	fmt.Println("\n Fetching data from the source database...")
+	results, err := mysqlClient.FetchAllData(tables)
 	if err != nil {
 		log.Fatalf("failed to fetch data %v", err)
 	}
-	fmt.Printf("Fetched data is: %v", data)
+	fmt.Printf("Fetched %d rows of data:", len(results))
 
+	//Handling target database
+	if *targetDB != "" {
+		fmt.Printf("Preparing to migrate data to %s.. ", *targetDB)
+		switch strings.ToLower(*targetDB) {
+		case "postgresql":
+			//TO DO import logic
+			fmt.Println("Postgres import not  yet implemented")
+		case "mongodb":
+			//TO Do import logic
+			fmt.Println("MongoDb logic not yet implemented")
+		default:
+			log.Fatalf("unsupported database target type %s", *targetDB)
+		}
+	}
+	fmt.Println("Migration Process completed!!")
 }
