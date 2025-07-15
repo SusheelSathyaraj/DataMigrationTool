e79b9b07ad93966141657071855676aea9e77c78
Susheel Sathyaraj <susheel.sathyaraj@gmail.com>
2025-07-15 23:56:49 +0530

    solving merge conflict

--- config/config.go
+++ config/config.go
@@ -25,15 +25,9 @@ type PostgreSQLConfig struct {
 
 // config struct to map config.yaml
 type Config struct {
-<<<<<<< HEAD
-	MySQL      MySQLConfig      `yaml:"mysql"`
-	PostgreSQL PostgreSQLConfig `yaml:"postgresql"`
-	FilePath   string           `yaml:"sqlfile_path"`
-=======
 	MySQL       MySQLConfig      `yaml:"mysql"`
 	PostgreSQL  PostgreSQLConfig `yaml:"postgresql"`
 	SQLFilePath string           `yaml:"sqlfile_path"`
->>>>>>> migration
 }
 
 func LoadConfig(filepath string) (*Config, error) {
