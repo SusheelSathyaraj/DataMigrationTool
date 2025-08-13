eaabd87192189034a52fd95a185414f34070ed5e
Susheel Sathyaraj <susheel.sathyaraj@gmail.com>
2025-08-13 22:54:04 +0530

    adds the premigration validation checks

--- validation/validator.go
+++ validation/validator.go
@@ -1,6 +1,8 @@
 package validation
 
 import (
+	"fmt"
+	"log"
 	"time"
 
 	"github.com/SusheelSathyaraj/DataMigrationTool/database"
@@ -30,4 +32,41 @@ func NewMigrationValidator(source, target database.DatabaseClient) *MigrationVai
 		TargetClient: target,
 		SampleSize:   100, //default samplesize
 	}
+}
+
+// performing validation checks before migration
+func (m *MigrationVaildator) PreMigrationValidation(tables []string) ([]ValidationResult, error) {
+	log.Println("Starting Premigration Validation ...")
+
+	var results []ValidationResult
+
+	for _, table := range tables {
+		result := ValidationResult{
+			TableName: table,
+			TimeStamp: time.Now(),
+		}
+
+		//checking if table is present and getting the row count
+		sourceData, err := m.SourceClient.FetchAllData([]string{table})
+		if err != nil {
+			result.IsValid = false
+			result.ErrorMessage = fmt.Sprintf("Failed to fetch data from the source table %s:%v", table, err)
+			results = append(results, result)
+			continue
+		}
+
+		result.RowCount = int64(len(sourceData))
+		result.IsValid = true
+
+		//getting samples for validation
+		sampleSize := m.SampleSize
+		if len(sourceData) < sampleSize {
+			sampleSize = len(sourceData)
+		}
+		result.SampleData = sourceData[:sampleSize]
+
+		log.Printf("Pre-Validation: Table %s contains %d rows", table, result.RowCount)
+		results = append(results, result)
+	}
+	return results, nil
 }
