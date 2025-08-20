3f61337f1a4df0e01730f2e58671848832d2b098
Susheel Sathyaraj <susheel.sathyaraj@gmail.com>
2025-08-20 23:21:00 +0530

    adds the postmigration validation to check integrity

--- validation/validator.go
+++ validation/validator.go
@@ -70,3 +70,71 @@ func (m *MigrationVaildator) PreMigrationValidation(tables []string) ([]Validati
 	}
 	return results, nil
 }
+
+// performing validation checks after migration completion
+func (m *MigrationVaildator) PostMigationValidation(tables []string, preValidationResults []ValidationResult) ([]ValidationResult, error) {
+	log.Println("Starting Post Migration Validation...")
+
+	var results []ValidationResult
+
+	//creating a map for quick lookupof pre-validation results
+	preResultMap := make(map[string]ValidationResult)
+	for _, result := range preValidationResults {
+		preResultMap[result.TableName] = result
+	}
+
+	for _, table := range tables {
+		result := ValidationResult{
+			TableName: table,
+			TimeStamp: time.Now(),
+		}
+
+		//getting target data
+		targetData, err := m.TargetClient.FetchAllData([]string{table})
+		if err != nil {
+			result.IsValid = false
+			result.ErrorMessage = fmt.Sprintf("Failed to fetch data from target table %s, %v", table, err)
+			results = append(results, result)
+			continue
+		}
+
+		result.RowCount = int64(len(targetData))
+
+		//comparing with source data count
+		preResult, exists := preResultMap[table]
+		if !exists {
+			result.IsValid = false
+			result.ErrorMessage = fmt.Sprintf("No prevalidation data found for table %s", table)
+			results = append(results, result)
+			continue
+		}
+
+		if result.RowCount != preResult.RowCount {
+			result.IsValid = false
+			result.ErrorMessage = fmt.Sprintf("Row count mismatch, expected source: %d, got target: %d", preResult.RowCount, result.RowCount)
+			results = append(results, result)
+			continue
+		}
+
+		//sample data validation
+		if len(targetData) > 0 {
+			sampleSize := m.SampleSize
+			if len(targetData) < sampleSize {
+				sampleSize = len(targetData)
+			}
+			result.SampleData = targetData[:sampleSize]
+
+			//Validating sample data integrity
+			if err := m.validateSampleDataIntegrity(preResult.SampleData, result.SampleData); err != nil {
+				result.IsValid = false
+				result.ErrorMessage = fmt.Sprintf("Data integrity Validation failed, %v ", err)
+				results = append(results, result)
+				continue
+			}
+		}
+		result.IsValid = true
+		log.Printf("Post-validation: Table %s successfully migrated with %d rows", table, result.RowCount)
+		results = append(results, result)
+	}
+	return results, nil
+}
