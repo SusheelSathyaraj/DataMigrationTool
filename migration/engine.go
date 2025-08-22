6f6e718f195aaa4f19ddc246c0f4c7d5ecbdd063
Susheel Sathyaraj <susheel.sathyaraj@gmail.com>
2025-08-22 17:29:51 +0530

    adds premigration validation to the migration

--- migration/engine.go
+++ migration/engine.go
@@ -1,6 +1,8 @@
 package migration
 
 import (
+	"fmt"
+	"log"
 	"time"
 
 	"github.com/SusheelSathyaraj/DataMigrationTool/database"
@@ -58,5 +60,35 @@ func NewMigrationEngine(config MigrationConfig, source, target database.Database
 		SourceClient: source,
 		TargetClient: target,
 		Validator:    validation.NewMigrationValidator(source, target),
+	}
+}
+
+// running the complete migration logic
+func (me *MigrationEngine) ExecuteMigration() (*MigrationResult, error) {
+	startTime := time.Now()
+
+	result := &MigrationResult{
+		StartTime: startTime,
+		Errors:    make([]string, 0),
+	}
+
+	log.Printf("Starting %s migation from %s to %s", me.Config.Mode, me.Config.SourceDb, me.Config.TargetDb)
+
+	//Step1: Premigration  validation
+	if me.Config.ValidateData {
+		preValidation, err := me.Validator.PreMigrationValidation(me.Config.Tables)
+		if err != nil {
+			return result, fmt.Errorf("pre-migration validation failed, %v", err)
+		}
+		result.PreValidation = preValidation
+
+		preValidationSummary := validation.GenerateValidationSummary(preValidation, startTime)
+		preValidationSummary.Print("Pre-Migration")
+
+		//checking for any failed tables validation
+		if preValidationSummary.InvalidTables > 0 {
+			return result, fmt.Errorf("pre-migration validation failed for %d tables", preValidationSummary.InvalidTables)
+		}
+
 	}
 }
