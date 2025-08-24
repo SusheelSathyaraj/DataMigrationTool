8cecbba1cd2ffb2dd404d8480e39d8ccafc41627
Susheel Sathyaraj <susheel.sathyaraj@gmail.com>
2025-08-24 21:20:03 +0530

    adds test for fetchalldata

---
+++ validation/validator_test.go
@@ -0,0 +1,45 @@
+package validation
+
+import (
+	"database/sql"
+	"errors"
+)
+
+// mockdatabase for testing
+type MockDatabaseClient struct {
+	mockData map[string][]map[string]interface{}
+	failOn   string //table name to fail on
+}
+
+func (m *MockDatabaseClient) Connect() error                                 { return nil }
+func (m *MockDatabaseClient) Close() error                                   { return nil }
+func (m *MockDatabaseClient) ExecuteQuery(query string) (*sql.Rows, error)   { return nil, nil }
+func (m *MockDatabaseClient) ImportData(data []map[string]interface{}) error { return nil }
+func (m *MockDatabaseClient) FetchAllDataConcurrently(tables []string, numWorkers int) ([]map[string]interface{}, error) {
+	return m.FetchAllData(tables)
+}
+func (m *MockDatabaseClient) ImportDataConcurrently(data []map[string]interface{}, batchSize int) error {
+	return m.ImportData(data)
+}
+
+func (m *MockDatabaseClient) FetchAllData(tables []string) ([]map[string]interface{}, error) {
+	if len(tables) == 0 {
+		return []map[string]interface{}{}, nil
+	}
+
+	var allData []map[string]interface{}
+	for _, table := range tables {
+		if table == m.failOn {
+			return nil, errors.New("mock error for table" + table)
+		}
+
+		if data, exists := m.mockData[table]; exists {
+			//add source table metadata
+			for i := range data {
+				data[i]["_source_table"] = table
+			}
+			allData = append(allData, data...)
+		}
+	}
+	return allData, nil
+}
